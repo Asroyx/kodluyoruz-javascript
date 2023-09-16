2625df215d2a9a11ee9b3ca14e3af1839274a7cd
Asroyx <mystoganza@hotmail.com>
2023-09-16 19:16:14 +0300

    feat: Modify the value inside a variable defined with 'let'

--- js/degisken-tanimlama.js
+++ js/degisken-tanimlama.js
@@ -19,5 +19,12 @@ let password = "1234";
 console.log(password);
 
 // Trying to use a variable without assigning a value
+
 console.log(fullName);
-let fullName = "Hakan Yalcinkaya";
+// let fullName = "Hakan Yalcinkaya"; This will result in an error
+var fullName = "Hakan Yalcinkaya"; // This is a correct
+
+// Modifying the value inside a variable defined with 'let'
+
+fullName = "Lorem Ipsum Dolor";
+console.log(fullName);
