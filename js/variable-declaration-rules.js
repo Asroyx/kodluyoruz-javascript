291368d83d98cc9ac2d1d6506e4263d34ca3fd43
Asroyx <mystoganza@hotmail.com>
2023-09-17 13:15:34 +0300

    docs: Add references to variable declaration rules in variable-declaration-rules.js

---
+++ js/variable-declaration-rules.js
@@ -0,0 +1,4 @@
+// ********** Variable Declaration Rules *****
+// https://www.w3schools.com/js/js_conventions.asp
+// https://google.github.io/styleguide/jsguide.html#features-local-variable-declarations
+// https://www.robinwieruch.de/javascript-naming-conventions
