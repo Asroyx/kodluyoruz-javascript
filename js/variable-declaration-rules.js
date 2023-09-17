ef9f283d33fdf824742dadeff4f709891c9352ef
Asroyx <mystoganza@hotmail.com>
2023-09-17 13:24:12 +0300

    docs: Add examples of camelCase usage in variable-declaration-rules.js

--- js/variable-declaration-rules.js
+++ js/variable-declaration-rules.js
@@ -2,3 +2,13 @@
 // https://www.w3schools.com/js/js_conventions.asp
 // https://google.github.io/styleguide/jsguide.html#features-local-variable-declarations
 // https://www.robinwieruch.de/javascript-naming-conventions
+
+// ### General Variable Declaration Rules
+// camelCase Usage:
+
+let fullname = ""; // This is not correct usage
+let kodluyoruzserverinfo = ""; // This is not correct usage
+let kodluyoruz_server_info = ""; // This is not correct usage
+let fullName = "";
+let kodluyoruzServerInfo = "";
+let kodluyoruzSERverInfo = ""; // This is not correct usage
