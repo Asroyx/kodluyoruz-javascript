8be5fa8386896fde638080a4fdd2e4c124f0e477
Asroyx <mystoganza@hotmail.com>
2023-09-18 21:36:19 +0300

    docs: Add number data types and KDV calculation example

---
+++ js/04-number-data-type-definition.js
@@ -0,0 +1,17 @@
+// ********** number **********
+// Defining the number data type:
+
+let price = 100;
+let tax = 0.18;
+let priceTax = price * tax;
+let total = price + priceTax;
+console.log(
+  "Price:",
+  price,
+  "KDV Rate:",
+  tax,
+  "KDV Amount:",
+  priceTax,
+  "Total Price:",
+  total
+);
