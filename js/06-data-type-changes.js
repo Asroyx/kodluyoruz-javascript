5137f5de256fb9d602842de4cbe755e6c38e92d4
Asroyx <mystoganza@hotmail.com>
2023-09-20 14:15:37 +0300

    docs: Add examples for converting string information to int and float with parse and number

--- js/06-data-type-changes.js
+++ js/06-data-type-changes.js
@@ -16,3 +16,21 @@ console.log(
     "hasPassword: ",
     typeof(hasPassword)
 );
+
+// Converting string information to int and float:
+
+let number1 = "11";
+number1 = parseInt(number1);
+console.log("number1: ", number1, typeof(number1));
+
+let number2 = "11px";
+number2 = parseInt(number2);
+console.log("number2: ", number2, typeof(number2));
+
+let number3 = "11.1";
+number3 = Number(number3);
+console.log("number3: ", number3, typeof(number3));
+
+let number4 = "11.4px";
+number4 = parseFloat(number4);
+console.log("number4: ", number4, typeof(number4));
