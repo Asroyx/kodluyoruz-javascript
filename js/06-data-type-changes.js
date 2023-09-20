5361c513184c7fb8cbafe03cf6b7f6d9320f8d8d
Asroyx <mystoganza@hotmail.com>
2023-09-20 14:02:35 +0300

    docs: Add an example of discovering data types using typeof

---
+++ js/06-data-type-changes.js
@@ -0,0 +1,18 @@
+// ********** Discovering Data Types and Data Type Conversions **********
+// Learning data types using typeof:
+let price = 111;
+let stringPrice = "111";
+let hasPassword = true;
+
+console.log(
+    "price: ",
+    typeof(price)
+);
+console.log(
+    "stringPrice: ",
+    typeof(stringPrice)
+);
+console.log(
+    "hasPassword: ",
+    typeof(hasPassword)
+);
