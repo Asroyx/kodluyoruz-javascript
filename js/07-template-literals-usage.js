aff8a37938dc01aedade1719f72ea9c226a87bcb
Asroyx <mystoganza@hotmail.com>
2023-09-22 23:29:54 +0300

    docs: Add template literals usage example

---
+++ js/07-template-literals-usage.js
@@ -0,0 +1,12 @@
+// *****Template Literals Usage **********
+// https://developer.mozilla.org/en-us/docs/Web/JavaScript/Reference/Template_literals
+
+let username = "hakan";
+const DOMAIN = "kodluyoruz.org";
+let email = username + "@" + DOMAIN;
+console.log(
+  "Hello",
+  username,
+  "welcome to our website, your email address is:",
+  email
+);
