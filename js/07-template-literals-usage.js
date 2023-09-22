cfa1a2da97b84d1fd8c0e05c7a33988112948b68
Asroyx <mystoganza@hotmail.com>
2023-09-22 23:38:03 +0300

    feat: Enhance code with template literals and added user information

--- js/07-template-literals-usage.js
+++ js/07-template-literals-usage.js
@@ -4,9 +4,16 @@
 let username = "hakan";
 const DOMAIN = "kodluyoruz.org";
 let email = username + "@" + DOMAIN;
-console.log(
-  "Hello",
-  username,
-  "welcome to our website, your email address is:",
-  email
-);
+
+// console.log("Hello",username,"welcome to our website, your email address is:",email);
+
+let info = `
+Hello ${username}, welcome to our website.
+Your email address: ${email}
+Email address length: ${email.length}
+Your debt: ${(2 + 3) * 10} TL
+Current time of the day: ${new Date().getHours()}
+Your short name: ${username.slice(0)}
+`;
+
+console.log(info);
