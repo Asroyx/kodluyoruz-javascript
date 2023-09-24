b121c0f02193ed9aea8ffaa7283ce4017ef7f93f
Asroyx <mystoganza@hotmail.com>
2023-09-24 23:49:08 +0300

    docs: Add example string data type operations

---
+++ js/08-string-data-type-operations.js
@@ -0,0 +1,5 @@
+// ********** String Data Type Operations **********
+// https://www.w3schools.com/jsref/jsref_obj_string.asp
+let email = "hakanyalcinkaya@gmail.com";
+let firstName = "hakan";
+let lastName = "YALCINKAYA";
