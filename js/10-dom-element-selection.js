d76327fc0dfaaca49af43efdf6a43b86f750a0ae
Asroyx <mystoganza@hotmail.com>
2023-09-27 22:06:56 +0300

    docs: Add an example for select all <h2> elements from the DOM

---
+++ js/10-dom-element-selection.js
@@ -0,0 +1,6 @@
+// ********** DOM Icinden Oge Secimi *********
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector
+
+let titles = document.getElementsByTagName("h2");
+console.log(titles);
