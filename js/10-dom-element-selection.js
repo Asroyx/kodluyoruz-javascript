cde57cad13a3ea6fc898175f28e5abd6b1000700
Asroyx <mystoganza@hotmail.com>
2023-09-28 21:47:05 +0300

    docs: Add an example for change the content of the element with id 'title' and display it in the console

--- js/10-dom-element-selection.js
+++ js/10-dom-element-selection.js
@@ -2,5 +2,7 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById
 // https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector
 
-let titles = document.getElementsByTagName("h2");
-console.log(titles);
+// let h2 = document.getElementsByTagName ('h2')
+let title = document.getElementById("title");
+title.innerHTML = "Changed Information";
+console.log(title.innerHTML);
