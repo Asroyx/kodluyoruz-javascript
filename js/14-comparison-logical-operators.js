a54cfc2736010590e6a6821dde3c973ef31977d0
Asroyx <mystoganza@hotmail.com>
2023-09-29 22:14:21 +0300

    docs: Add an example for equality comparisons (== and ===)

---
+++ js/14-comparison-logical-operators.js
@@ -0,0 +1,12 @@
+//********** Comparison Operators and Logical Operators *****
+// Reference: https://www.w3schools.com/js/js_comparisons.asp
+
+let price = "100";
+
+// == Equal to (value only)
+console.log("price == 1:", price == 1);
+console.log("price == 100:", price == 100);
+
+// === Equal to (value and type)
+console.log("price === 1:", price === 1);
+console.log("price === 100:", price === 100);
