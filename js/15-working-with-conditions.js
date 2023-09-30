fd209fd4eaf69a99b7c699241f264f042f0d8dc9
Asroyx <mystoganza@hotmail.com>
2023-09-30 23:28:22 +0300

    docs: Add example for display user information if a username is provided

---
+++ js/15-working-with-conditions.js
@@ -0,0 +1,10 @@
+//********** Working with Conditions **********
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/if...else
+
+let username = prompt("Enter Your Username:");
+// Display the user's name on the screen if user information is available
+
+// if (username.length > 0) { console.log(username); }
+if (username.length > 0) {
+  console.log(`Your User Information: ${username}`);
+}
