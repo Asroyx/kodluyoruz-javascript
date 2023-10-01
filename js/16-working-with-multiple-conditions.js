93000d16fd1ac207d75ed8c3b251a386b2c9aefb
Asroyx <mystoganza@hotmail.com>
2023-10-01 23:18:35 +0300

    docs: Add example for check eligibility for a driver's license based on user input

---
+++ js/16-working-with-multiple-conditions.js
@@ -0,0 +1,8 @@
+// ********** Working with Multiple Conditions **********
+
+let userName = prompt("Your Username:");
+let age = prompt("Your Age:");
+
+if (userName && age >= 18) {
+  console.log("You can get a driver's license");
+}
