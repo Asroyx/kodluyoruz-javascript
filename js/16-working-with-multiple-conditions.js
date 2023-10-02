d418e12660ed7254c25d94ccdd34be72d1fd8c4d
Asroyx <mystoganza@hotmail.com>
2023-10-02 15:30:41 +0300

    feat: Enhance user eligibility check for a driver's license and show info in html tag

--- js/16-working-with-multiple-conditions.js
+++ js/16-working-with-multiple-conditions.js
@@ -2,7 +2,12 @@
 
 let userName = prompt("Your Username:");
 let age = prompt("Your Age:");
+let info = document.querySelector("#info");
 
-if (userName && age >= 18) {
-  console.log("You can get a driver's license");
+if (!userName || !age) {
+  info.innerHTML = "Please provide your Username and Age correctly.";
+} else if (age >= 18) {
+  info.innerHTML = "You are eligible for a driver's license.";
+} else {
+  info.innerHTML = "Sorry, you are not eligible for a driver's license.";
 }
