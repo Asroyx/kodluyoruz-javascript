07a649c77011da00626680d22bb2aa0cbd6076ca
Asroyx <mystoganza@hotmail.com>
2023-10-05 15:03:43 +0300

    docs: Add simple example functions hello() and helloWorld() to learn about functions in JavaScript

---
+++ js/19-first-function.js
@@ -0,0 +1,12 @@
+// Define our first function:
+
+function hello() {
+  console.log("Merhaba");
+  helloWorld();
+}
+
+function helloWorld() {
+  console.log("Hello World");
+}
+
+hello(); // Working
