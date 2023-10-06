27bdeb63c6562a4ffb420ef673f809df2302ded5
Asroyx <mystoganza@hotmail.com>
2023-10-06 23:50:15 +0300

    docs: Add example for a function to greet the user by name

---
+++ js/21-arrow-function-usage.js
@@ -0,0 +1,8 @@
+// Arrow function Usage
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+
+function hello(name) {
+  console.log(`Hello ${name}`);
+}
+
+hello("Javascript");
