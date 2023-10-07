729104bf355d62ff87892e107aa7f7ac1f71488f
Asroyx <mystoganza@hotmail.com>
2023-10-07 23:58:54 +0300

    docs: Add example for event listener to ID greeting to toggle text color on mouseover

--- js/22-javaScript-dom-events.js
+++ js/22-javaScript-dom-events.js
@@ -2,10 +2,18 @@
 // https://www.w3schools.com/jsref/dom_obj_event.asp
 // https://developer.mozilla.org/en-US/docs/Web/Events
 
+// const greeting = document.querySelector("#greeting");
+// greeting.addEventListener("click", domClick);
+
+// function domClick() {
+//   console.log("Clicked!");
+//   this.style.color = this.style.color === "red" ? "black" : "red";
+// }
+
 const greeting = document.querySelector("#greeting");
-greeting.addEventListener("click", domClick);
+greeting.addEventListener("mouseover", domClick);
 
 function domClick() {
-  console.log("Clicked!");
+  console.log("Event handled!");
   this.style.color = this.style.color === "red" ? "black" : "red";
 }
