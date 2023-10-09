eb354c1c3eb204930f008f6f3dc54e83a2388f13
Asroyx <mystoganza@hotmail.com>
2023-10-09 22:31:13 +0300

    update: Improve event listener implementation for increase and decrease buttons

--- js/23-functions-section-final-exercise.js
+++ js/23-functions-section-final-exercise.js
@@ -12,4 +12,6 @@ decreaseDOM.addEventListener("click", clickEvent);
 
 function clickEvent() {
   console.log(this.id);
+  this.id == "increase" ? (counter += 1) : (counter -= 1);
+  counterDOM.innerHTML = counter;
 }
