e7848bdb87a4ce4be4ecd9fe2650c33e6db37617
Asroyx <mystoganza@hotmail.com>
2023-10-15 23:08:30 +0300

    Add scoreInputDOM value to localStorage to track the user's score

--- js/27-form-submission-management.js
+++ js/27-form-submission-management.js
@@ -6,6 +6,8 @@ formDOM.addEventListener("submit", formSubmit);
 function formSubmit(event) {
   event.preventDefault();
   console.log("Process completed");
+
   let scoreInputDOM = document.querySelector("#score");
   console.log(scoreInputDOM);
+  localStorage.setItem("score", scoreInputDOM.value);
 }
