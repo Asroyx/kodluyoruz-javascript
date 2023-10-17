01a503d2a389f0c1a51f8b9e17da92c00ee8b987
Asroyx <mystoganza@hotmail.com>
2023-10-17 23:22:43 +0300

    Move deleted code to a new JS file for further edits

--- js/27-form-submission-management.js
+++ js/27-form-submission-management.js
@@ -6,8 +6,4 @@ formDOM.addEventListener("submit", formSubmit);
 function formSubmit(event) {
   event.preventDefault();
   console.log("Process completed");
-
-  let scoreInputDOM = document.querySelector("#score");
-  console.log(scoreInputDOM);
-  localStorage.setItem("score", scoreInputDOM.value);
 }
