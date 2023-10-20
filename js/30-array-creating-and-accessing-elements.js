a215177f86bf081b1084d93d338ad7f479ed3fd8
Asroyx <mystoganza@hotmail.com>
2023-10-20 23:41:20 +0300

    Add basic array operations

---
+++ js/30-array-creating-and-accessing-elements.js
@@ -0,0 +1,16 @@
+// array - working with arrays
+
+// creating an array
+
+let domain = "kodluyoruz";
+let isActive = false;
+let items = [1, 2, 3, isActive, domain];
+console.log(items);
+
+// getting the number of elements in an array
+
+// calling the first element in an array
+
+// calling the last element in an array
+
+// checking if the information in a variable is an array
