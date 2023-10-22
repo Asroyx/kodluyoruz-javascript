73affc0d3672b1ac8c55e4a3a8a073c21b52d266
Asroyx <mystoganza@hotmail.com>
2023-10-22 23:45:34 +0300

    Add an array with 5 elements

---
+++ js/31-adding-and-removing-elements-to-array.js
@@ -0,0 +1,5 @@
+// Adding and removing elements from an array
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+let items = [10, 20, 30, 40, 50];
+console.log("items - initial state: ", items);
