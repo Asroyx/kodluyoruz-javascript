ff92b7c4db9a2a03dd048349c4297eb46a0f325f
Asroyx <mystoganza@hotmail.com>
2023-10-25 23:32:14 +0300

    Add femaleUsers array to the beginning of the items array with the unshift() method

---
+++ js/32-commonly-used-array-methods.js
@@ -0,0 +1,13 @@
+// Commonly used array methods and array within array
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+// https://www.w3schools.com/js/jS_array_methods…asp
+
+const items = [1, 2, 3, 4, 5];
+
+// Array within array:
+const femaleUsers = ["Ayse", "Hulya", "Merve"];
+const maleUsers = ["Ahmet", "Hasan", "Mehmet"];
+
+items.unshift(femaleUsers);
+
+console.log(items);
