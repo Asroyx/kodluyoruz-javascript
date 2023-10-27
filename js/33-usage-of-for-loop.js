127a205f9ef58f757fc5aac06c2dae1af1f53f58
Asroyx <mystoganza@hotmail.com>
2023-10-27 21:43:09 +0300

    Add for loop to print user indexes

---
+++ js/33-usage-of-for-loop.js
@@ -0,0 +1,11 @@
+// For Loop
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration
+// for (initialization; condition; increment) {
+//   statements
+// }
+
+const users = ["Lorem", "Ipsum", "Dolor"];
+
+for (let index = 0; index < users.length; index++) {
+  console.log(index);
+}
