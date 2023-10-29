748451c22c0cb45cc4718f081938d08463352461
Asroyx <mystoganza@hotmail.com>
2023-10-29 22:46:44 +0300

    Add while loop for user prompt

--- js/35-usage-of-while-loop.js
+++ js/35-usage-of-while-loop.js
@@ -1,9 +1,16 @@
 // While Loop
 // https://www.w3schools.com/js/js_loop_while.asp
 
-let counter = 0;
+// let counter = 0;
 
-while (counter < 10) {
-  console.log(counter);
-  counter++;
+// while (counter < 10) {
+//   console.log(counter);
+//   counter++;
+// }
+
+let userName;
+
+while (!userName) {
+  userName = prompt("Enter your username");
+  console.log(userName);
 }
