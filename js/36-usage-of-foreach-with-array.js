57c5b07d80c87bceaab7c25709c4d259361cd7fe
Asroyx <mystoganza@hotmail.com>
2023-10-30 23:14:12 +0300

    Add array declaration for PRODUCTS

---
+++ js/36-usage-of-foreach-with-array.js
@@ -0,0 +1,14 @@
+// Array forEach Method
+// Reference:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+// https://www.w3schools.com/jsref/jsref_foreach.asp
+
+const PRODUCTS = [
+  "Laptop",
+  "Phone",
+  "Speaker",
+  "Desktop PC",
+  "Server",
+  "Mouse",
+  "Keyboard",
+];
