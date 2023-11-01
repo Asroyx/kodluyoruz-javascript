ab7ebcdc3519a5ed66b3f972579034fbafe51944
Asroyx <mystoganza@hotmail.com>
2023-11-01 23:48:44 +0300

    Add case-insensitive array for USERS

---
+++ js/38-usage-of-array-map.js
@@ -0,0 +1,8 @@
+// Array Map Usage
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+
+const USERS = ["AYSE", "MehMet", "TugCE", "AkSEL"];
+
+// Keep the original name in userName,
+// Capitalize the first letter in shortName (A.)
+// Capitalize the first letter in newName
