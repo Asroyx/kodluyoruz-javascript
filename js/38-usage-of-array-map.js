a1efa49afcb97b4289a7672271d10c3c1e164bb5
Asroyx <mystoganza@hotmail.com>
2023-11-02 23:58:23 +0300

    Add an object for each user with userName, shortName and newName properties

--- js/38-usage-of-array-map.js
+++ js/38-usage-of-array-map.js
@@ -9,3 +9,13 @@ const USERS = ["AYSE", "MehMet", "TugCE", "AkSEL"];
 
 const NEW_USERS = USERS.map((user) => user.toLowerCase());
 console.log(NEW_USERS);
+
+const USERS_OBJ = USERS.map((user) => {
+  return {
+    userName: user,
+    shortName: `${user[0]}.`,
+    newName: `${user[0].toUpperCase()}${user.slice(1).toLowerCase()}`,
+  };
+});
+
+console.log(USERS_OBJ);
