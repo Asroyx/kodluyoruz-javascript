224dc4920fe1a6c227a520999da56cab81c17f3c
Asroyx <mystoganza@hotmail.com>
2023-11-07 23:20:33 +0300

    Add an object named laptop1

---
+++ js/40-object-key-value-usage.js
@@ -0,0 +1,9 @@
+// Object Key - Value:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Working_with_Objects
+
+// [key1:1, key2:2, key3:3,]
+let laptop1 = {
+  brand: "Apple",
+  model: "MacBook Pro",
+  "2kg": 2,
+};
