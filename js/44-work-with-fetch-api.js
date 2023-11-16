2fd1dda1d83c8bfb17ccf7ac839a4a86d59613ac
Asroyx <mystoganza@hotmail.com>
2023-11-16 23:53:51 +0300

    Fetch and log 'data/settings.json' JSON, highlighting user name extraction

--- js/44-work-with-fetch-api.js
+++ js/44-work-with-fetch-api.js
@@ -2,9 +2,8 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
 
 fetch("data/settings.json")
-  .then(response => {
-    return response.json();
-  })
-  .then(responseJson => {
+  .then((response) => response.json())
+  .then((responseJson) => {
     console.log(responseJson);
+    console.log(responseJson.userName);
   });
