a96c1fcfb40be46ddb333a545861cf5f4ba959a4
Asroyx <mystoganza@hotmail.com>
2023-11-16 23:42:53 +0300

    Implement data retrieval for settings.json using the fetch API

--- js/44-work-with-fetch-api.js
+++ js/44-work-with-fetch-api.js
@@ -1,2 +1,6 @@
 // Working with fetch api
 // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+
+fetch("data/settings.json").then((response) => {
+  console.log(response);
+});
