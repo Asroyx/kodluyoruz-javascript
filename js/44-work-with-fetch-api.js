cde1a91299cb6e636f47ebb10b4167eb4ff49c5c
Asroyx <mystoganza@hotmail.com>
2023-11-16 23:48:20 +0300

    Async fetch and log 'data/settings.json' JSON response

--- js/44-work-with-fetch-api.js
+++ js/44-work-with-fetch-api.js
@@ -1,6 +1,10 @@
 // Working with fetch api
 // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
 
-fetch("data/settings.json").then((response) => {
-  console.log(response);
-});
+fetch("data/settings.json")
+  .then(response => {
+    return response.json();
+  })
+  .then(responseJson => {
+    console.log(responseJson);
+  });
